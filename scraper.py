79c9c04225947f5a35fd58d40f12c77e37618aac
Yifei Hu <huyifei23@gmail.com>
2016-01-15 07:58:22 -0600

    Working towards tracking minutes of each lineup. Still needs some work.

--- scraper.py
+++ scraper.py
@@ -12,10 +12,12 @@ def scrape(splited_text):
 	RICE_IS_HOME = False
 	current_lineup = get_starting_lineup("Koulechov", "Drone", "Guercy", "Mency","Evans")
 	print current_lineup
-	current_stint = {"points scored": 0, "points allowed": 0,
-					"time": datetime.time(0, 0, 0)}
+	current_stint = {"lineup": tuple(current_lineup), "points scored": 0, "points allowed": 0,
+					"begin time": datetime.timedelta(0, 0, minutes=20), "end time": datetime.timedelta(0, 0, minutes=20),
+					"lasting time": None}
+	#print current_stint
 	pattern = re.compile(":[^A-Za-z]+")
-	stints = {tuple(current_lineup): current_stint}
+	stints = []
 	for line in splited_text:
 	 	is_log = re.search(pattern, line)
 	 	if is_log:
@@ -26,8 +28,10 @@ def scrape(splited_text):
 			 	if is_time:
 			 		game_time_index = splited_line.index(element)
 			 		game_time = element.split(":")
-			 		game_time = datetime.time(0, int(game_time[0]), int(game_time[1]))
+			 		game_time = datetime.timedelta(0, int(game_time[1]), minutes=int(game_time[0]))
 			 		#print game_time
+			 		current_stint["end time"] = game_time
+			 		current_stint["lasting time"] = current_stint["begin time"] - current_stint["end time"]
 			 		if RICE_IS_HOME:
 			 			splited_line = splited_line[:game_time_index]
 			 		else:
@@ -42,13 +46,43 @@ def scrape(splited_text):
 			 				current_lineup.remove(player_name)
 			 			if len(current_lineup) == 5:
 			 				#print current_stint
-			 				if tuple(current_lineup) in stints:
-			 					stints[tuple(current_lineup)]["time"] += game_time
-			 				else:
-			 					stints[tuple(current_lineup)] = {"points scored": 0, "points allowed": 0,
-																"time": datetime.time(0, 0, 0)}
-	for stint in stints.values():
-	 	print stint  		 				
+			 				current_stint = {"lineup": tuple(current_lineup), "points scored": 0, "points allowed": 0,
+											"begin time": datetime.timedelta(0, game_time.total_seconds()),
+											"end time": datetime.timedelta(0, game_time.total_seconds()),
+											"lasting time": None}
+							stints.append(current_stint)
+			 				# if tuple(current_lineup) in stints:
+			 				# 	stints[tuple(current_lineup)]["time"] += game_time
+			 				# else:
+			 				#  	stints[tuple(current_lineup)] = {"points scored": 0, "points allowed": 0,
+								# 								"time": datetime.timedelta(0, 0, 0)}
+	return stints
+	# total_time = 0
+	# for stint in stints:
+	# 	total_time += stint["lasting time"].total_seconds()
+	# print total_time  		 				
+
+def format_stints(stints):
+	new_stints = []
+	existing_lineups = []
+	for stint in stints:
+		if stint["lineup"] in existing_lineups:
+			for new_stint in new_stints:
+				if new_stint["lineup"] == stint["lineup"]:
+					new_stint["points scored"] += stint["points scored"]
+					new_stint["points allowed"] += stint["points allowed"]
+					new_stint["lasting time"] += stint["lasting time"]
+		else:
+			new_stints.append(dict(stint))
+			existing_lineups.append(tuple(stint["lineup"]))
+	total_time = 0
+	for stint in new_stints:
+		total_time += stint["lasting time"].total_seconds()
+		stint["lasting time"] = str(stint["lasting time"])
+		del stint["begin time"]
+		del stint["end time"]
+	print total_time
+	return new_stints
 
 def main():
 	url = "http://www.riceowls.com/sports/m-baskbl/stats/2015-2016/rice1113.html"
@@ -65,9 +99,17 @@ def main():
 	second_half_text = text[divider_index:]
 
 	# Scrape them separately 
-	scrape(first_half_text)
+	stint1 = scrape(first_half_text)
 	print "hello"
-	scrape(second_half_text)
+	stint1.extend(scrape(second_half_text))
+	stint2 = format_stints(stint1)
+	for stint in stint2:
+		print stint
 					
 
 main()
+
+
+# 1 Use timedelta instead of time to represent t in game.
+# 2 Given a new stint, get its t0 and t1, whose difference will tell us how long did the stint last on the court.
+# Instead of having multiple stints with the same lineup, we aggregate the minutes, points, etc. with one single stint, because ultimately that's what we want.
