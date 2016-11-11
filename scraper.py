60cf3ebc998f5951bc8ac154e204eb211f87b71d
YifeiHu <huyifei23@gmail.com>
2016-11-11 13:03:17 -0600

    covers all cases.

--- scraper.py
+++ scraper.py
@@ -55,13 +55,6 @@ def scrape(split_text, lineup, rice_is_home, points_scored=0, points_allowed=0):
                     game_time = datetime.timedelta(0, int(game_time[1]), minutes=int(game_time[0]))
                     current_stint["end time"] = game_time
                     current_stint["lasting time"] = current_stint["begin time"] - current_stint["end time"]
-                    chopped = True   
-                    if RICE_IS_HOME:
-                        rice_split_line = split_line[:game_time_index] 
-                        opp_split_line = split_line[game_time_index + 1:]
-                    else:
-                        rice_split_line = split_line[game_time_index + 1:]
-                        opp_split_line = split_line[:game_time_index] 
                     if len(rice_split_line) != 0 and "SUB" in rice_split_line:
                         if rice_split_line[1] == "IN":
                             player_name = str(rice_split_line[3][:-1])
@@ -86,124 +79,88 @@ def scrape(split_text, lineup, rice_is_home, points_scored=0, points_allowed=0):
                             else:
                                 current_stint["initial points scored"] = away_score
                                 current_stint["initial points allowed"] = home_score
-                if RICE_IS_HOME:
-                    game_time_txt = filter(lambda x : re.match(time_pattern, x), split_line)
-                    if game_time_txt == [] or len(game_time_txt) > 1:
-                        break
-                    else:
-                        game_time_txt = game_time_txt[0]
+
+                game_time_txt = filter(lambda x : re.match(time_pattern, x), split_line)
+                if len(game_time_txt) != 1:
+                    break
+                else:
+                    game_time_txt = game_time_txt[0]
                     game_time_index = split_line.index(game_time_txt) # find game time
+                if RICE_IS_HOME:
                     rice_split_line = split_line[:game_time_index] 
                     opp_split_line = split_line[game_time_index + 1:]
-                    missed_shot = element in rice_split_line and re.match(missed_pattern, element) and rice_split_line[rice_split_line.index(element) + 1] != "FT"
-                    made_shot = element in rice_split_line and re.match(made_pattern, element) and rice_split_line[rice_split_line.index(element) + 1] != "FT"
-                    if missed_shot:
-                        # We don't want to count free throws.
-                        current_stint["field goal attempt"] += 1
-                        is_3 = rice_split_line[rice_split_line.index(element) + 1] == "3"
-                        if is_3:
-                            current_stint["three point attempt"] += 1
-                    elif made_shot:
-                        current_stint["field goal attempt"] += 1
-                        current_stint["field goal made"] += 1
-                        is_3 = rice_split_line[rice_split_line.index(element) + 1] == "3"
-                        if is_3:
-                            current_stint["three point attempt"] += 1
-                            current_stint["three point made"] += 1
+                else:
+                    rice_split_line = split_line[game_time_index + 1:]
+                    opp_split_line = split_line[:game_time_index]
+                    # We don't want to count free throws.
+                missed_shot = element in rice_split_line and re.match(missed_pattern, element) and rice_split_line[rice_split_line.index(element) + 1] != "FT"
+                made_shot = element in rice_split_line and re.match(made_pattern, element) and rice_split_line[rice_split_line.index(element) + 1] != "FT"
+                if missed_shot:
+                    current_stint["field goal attempt"] += 1
+                    is_3 = rice_split_line[rice_split_line.index(element) + 1] == "3"
+                    if is_3:
+                        current_stint["three point attempt"] += 1
+                elif made_shot:
+                    current_stint["field goal attempt"] += 1
+                    current_stint["field goal made"] += 1
+                    is_3 = rice_split_line[rice_split_line.index(element) + 1] == "3"
+                    if is_3:
+                        current_stint["three point attempt"] += 1
+                        current_stint["three point made"] += 1
 
-                    o_missed_shot = element in opp_split_line and re.match(missed_pattern, element) and opp_split_line[opp_split_line.index(element) + 1] != "FT"
-                    o_made_shot = element in opp_split_line and re.match(made_pattern, element) and opp_split_line[opp_split_line.index(element) + 1] != "FT"
-                    if o_missed_shot:
-                        current_stint["shots against"] += 1
-                        o_is_3 = opp_split_line[opp_split_line.index(element) + 1] == "3"
-                        if o_is_3:
-                            current_stint["threes against"] += 1
-                    elif o_made_shot:
-                        current_stint["shots against"] += 1
-                        current_stint["made shots against"] += 1
-                        o_is_3 = opp_split_line[opp_split_line.index(element) + 1] == "3"
-                        if o_is_3:
-                            current_stint["threes against"] += 1
-                            current_stint["made threes against"] += 1
+                o_missed_shot = element in opp_split_line and re.match(missed_pattern, element) and opp_split_line[opp_split_line.index(element) + 1] != "FT"
+                o_made_shot = element in opp_split_line and re.match(made_pattern, element) and opp_split_line[opp_split_line.index(element) + 1] != "FT"
+                if o_missed_shot:
+                    current_stint["shots against"] += 1
+                    o_is_3 = opp_split_line[opp_split_line.index(element) + 1] == "3"
+                    if o_is_3:
+                        current_stint["threes against"] += 1
+                elif o_made_shot:
+                    current_stint["shots against"] += 1
+                    current_stint["made shots against"] += 1
+                    o_is_3 = opp_split_line[opp_split_line.index(element) + 1] == "3"
+                    if o_is_3:
+                        current_stint["threes against"] += 1
+                        current_stint["made threes against"] += 1
 
 
-                    if re.match(rebound_pattern, element):
-                        if element in rice_split_line:
-                            off_reb = rice_split_line[rice_split_line.index(element) + 1] == "(OFF)" and rice_split_line[rice_split_line.index(element) + 3] != "(DEADBALL)"
-                            def_reb = rice_split_line[rice_split_line.index(element) + 1] == "(DEF)" and rice_split_line[rice_split_line.index(element) + 3] != "(DEADBALL)"
-                            if off_reb:
-                                    current_stint["offensive rebound"] += 1
-                            elif def_reb:
-                                current_stint["defensive rebound"] += 1
-                        elif element in opp_split_line:
-                            o_off_reb = opp_split_line[opp_split_line.index(element) + 1] == "(OFF)" and opp_split_line[opp_split_line.index(element) + 3] != "(DEADBALL)"
-                            o_def_reb = opp_split_line[opp_split_line.index(element) + 1] == "(DEF)" and opp_split_line[opp_split_line.index(element) + 3] != "(DEADBALL)"
-                            if o_off_reb:
-                                current_stint["o-rebounds allowed"] += 1
-                            elif o_def_reb:
-                                current_stint["d-rebounds allowed"] += 1
-
-                    is_assist = re.match(assist_pattern, element)
-                    if is_assist and element in rice_split_line:
-                        current_stint["assist"] += 1
-                    is_block = re.match(block_pattern, element)
-                    if is_block:
-                        if element in rice_split_line:
-                            current_stint["block"] += 1
-                        elif element in opp_split_line:
-                            current_stint["opponent block"] += 1
-                    is_steal = re.match(steal_pattern, element)
-                    if is_steal and element in rice_split_line:
-                        current_stint["steal"] += 1
-                    is_turnover = re.match(turnover_pattern, element)
-                    if is_turnover and element in rice_split_line:
-                        current_stint["turnover"] += 1
-                    is_foul = re.match(foul_pattern, element)
-                    if is_foul:
-                        if element in rice_split_line:
-                            current_stint["foul"] += 1
-                        elif element in opp_split_line:
-                            current_stint["fouls drawn"] += 1
-
-                elif chopped:
-                    missed_shot = re.match(missed_pattern, element)
-                    made_shot = re.match(made_pattern, element)
-
-                    if missed_shot and rice_split_line[rice_split_line.index(element) + 1] != "FT":
-                        current_stint["field goal attempt"] += 1
-                        is_3 = rice_split_line[rice_split_line.index(element) + 1] == "3"
-                        if is_3:
-                            current_stint["three point attempt"] += 1
-                    elif made_shot and rice_split_line[rice_split_line.index(element) + 1] != "FT":
-                        current_stint["field goal attempt"] += 1
-                        current_stint["field goal made"] += 1 
-                        is_3 = rice_split_line[rice_split_line.index(element) + 1] == "3"
-                        if is_3:
-                            current_stint["three point attempt"] += 1
-                            current_stint["three point made"] += 1
-
-                    if re.match(rebound_pattern, element):
+                if re.match(rebound_pattern, element):
+                    if element in rice_split_line:
                         off_reb = rice_split_line[rice_split_line.index(element) + 1] == "(OFF)" and rice_split_line[rice_split_line.index(element) + 3] != "(DEADBALL)"
                         def_reb = rice_split_line[rice_split_line.index(element) + 1] == "(DEF)" and rice_split_line[rice_split_line.index(element) + 3] != "(DEADBALL)"
                         if off_reb:
-                            current_stint["offensive rebound"] += 1
+                                current_stint["offensive rebound"] += 1
                         elif def_reb:
                             current_stint["defensive rebound"] += 1
-                    is_assist = re.match(assist_pattern, element)
-                    if is_assist and element in rice_split_line:
-                        current_stint["assist"] += 1
-                    is_block = re.match(block_pattern, element)
-                    if is_block and element in rice_split_line:
+                    elif element in opp_split_line:
+                        o_off_reb = opp_split_line[opp_split_line.index(element) + 1] == "(OFF)" and opp_split_line[opp_split_line.index(element) + 3] != "(DEADBALL)"
+                        o_def_reb = opp_split_line[opp_split_line.index(element) + 1] == "(DEF)" and opp_split_line[opp_split_line.index(element) + 3] != "(DEADBALL)"
+                        if o_off_reb:
+                            current_stint["o-rebounds allowed"] += 1
+                        elif o_def_reb:
+                            current_stint["d-rebounds allowed"] += 1
+
+                is_assist = re.match(assist_pattern, element)
+                if is_assist and element in rice_split_line:
+                    current_stint["assist"] += 1
+                is_block = re.match(block_pattern, element)
+                if is_block:
+                    if element in rice_split_line:
                         current_stint["block"] += 1
-                    is_steal = re.match(steal_pattern, element)
-                    if is_steal and element in rice_split_line:
-                        current_stint["steal"] += 1
-                    is_turnover = re.match(turnover_pattern, element)
-                    if is_turnover and element in rice_split_line:
-                        current_stint["turnover"] += 1
-                    is_foul = re.match(foul_pattern, element)
-                    if is_foul and element in rice_split_line:
+                    elif element in opp_split_line:
+                        current_stint["opponent block"] += 1
+                is_steal = re.match(steal_pattern, element)
+                if is_steal and element in rice_split_line:
+                    current_stint["steal"] += 1
+                is_turnover = re.match(turnover_pattern, element)
+                if is_turnover and element in rice_split_line:
+                    current_stint["turnover"] += 1
+                is_foul = re.match(foul_pattern, element)
+                if is_foul:
+                    if element in rice_split_line:
                         current_stint["foul"] += 1
+                    elif element in opp_split_line:
+                        current_stint["fouls drawn"] += 1
     current_stint["lasting time"] = current_stint["begin time"]
     stints.append(current_stint)
     return stints
@@ -361,23 +318,24 @@ def run(url, half_time_points_scored, half_time_points_allowed, rice_is_home, li
 
 
 if __name__ == "__main__":
-    url = "http://www.riceowls.com/sports/m-baskbl/stats/2015-2016/rice0220.html"
-    hps = 52
-    hpa = 34
-    rice_is_home = True
-    overtime = False
-    ops = 0
+    # away and overtime case
+    url = "http://www.riceowls.com/sports/m-baskbl/stats/2015-2016/rice0225.html"
+    hps = 28
+    hpa = 30
+    rice_is_home = False
+    overtime = True
+    ops = 66
     starting_lineup = set(["Koulechov", "Drone", "Guercy", "Cashaw", "Evans"])
     if not overtime:
         run(url, hps, hpa, rice_is_home, starting_lineup)
     else:
         run(url, hps, hpa, rice_is_home, starting_lineup, ops)
-
-# away & overtime case
-    # url = "http://www.riceowls.com/sports/m-baskbl/stats/2015-2016/rice0225.html"
-    # hps = 28
-    # hpa = 30
-    # rice_is_home = False
-    # overtime = True
-    # ops = 66
-    # starting_lineup = set(["Koulechov", "Drone", "Guercy", "Cashaw", "Evans"])
+ 
+# home case
+url = "http://www.riceowls.com/sports/m-baskbl/stats/2015-2016/rice0220.html"
+hps = 52
+hpa = 34
+rice_is_home = True
+overtime = False
+ops = 0
+starting_lineup = set(["Koulechov", "Drone", "Guercy", "Cashaw", "Evans"])
